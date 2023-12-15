fd742461c3be25daee908478ba35a4bddf163dab
Nickolas Faquini <101142440+Nickolas2019@users.noreply.github.com>
2023-12-15 14:12:42 -0300

    completeTodo

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -27,12 +27,24 @@ function App() {
     setTodos(newTodos);
   }
 
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null
+      );
+      setTodos(filteredTodos);
+  }
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo)
+    setTodos(newTodos)
+  }
+
   return (
     <div className='App'>
       <h1>Lista de Tarefas</h1>
       <div className='Todo--list'>
         {todos.map((todo) => 
-          <Todo key={todo.id} todo={todo}/>
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         )}
       </div>
       <TodoForm addTodo={addTodo}/>
--- TodoList/src/components/Todo.jsx
+++ TodoList/src/components/Todo.jsx
@@ -1,24 +1,27 @@
 import PropTypes from 'prop-types';
 
-function Todo({todo}) {
+function Todo({todo, removeTodo, completeTodo}) {
   return (
-    <div className='Todo'>
+    <div className='Todo' style={{textDecoration: todo.isCompleted ? "line-through" : null}}>
             <div className='Todo--content'>
               <p>{todo.text}</p>
               <p>{todo.category}</p>
             </div>
             <div>
-              <button>Completar tarefa</button>
-              <button>x</button>
+              <button onClick={() => completeTodo(todo.id)}>Completar tarefa</button>
+              <button onClick={() => removeTodo(todo.id)}>x</button>
             </div>
           </div>
   )
 }
 
 Todo.propTypes = {
+  removeTodo: PropTypes.func.isRequired,
+  completeTodo: PropTypes.func.isRequired,
   todo: PropTypes.shape({
     text: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
   }).isRequired,
 };
 
